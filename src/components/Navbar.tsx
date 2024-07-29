1420332cb4d9507d996f93d46998588f596efd59
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-07-29 11:35:17 +0200

    added social media icons
    replaced awesome font icons with proper icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 right-0 w-full text-white p-4">
+        <nav className="fixed top-6 right-8 w-full text-white">
           <ul className="flex text-2xl justify-end space-x-12">
             <li><Link href="projects">Projects</Link></li>
             <li><Link href="wisdom">Wisdom</Link></li>
--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
-// TODO - replace with svg icons, as I don't like these
-const SocialMediaIcons = () => {
+export default function SocialMediaIcons() {
     return (
-        <div className="fixed bottom-4 right-4 flex flex-col space-y-4">
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-red-500">
-                <i className="fab fa-youtube"></i>
+        <div className="fixed bottom-8 right-8 flex flex-col space-y-8">
+            <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+                <Image src="/icons/x-icon.svg" alt="X" width={0} height={0}
+                       style={{ width: '36px', height: 'auto' }} className="hover:opacity-75"/>
             </a>
-            <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-blue-500">
-                <i className="fab fa-discord"></i>
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                <Image src="/icons/youtube-icon.svg" alt="YouTube" width={0} height={0}
+                       style={{ width: '36px', height: 'auto' }} className="hover:opacity-75"/>
             </a>
-            <a href="https://x.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-blue-400">
-                <i className="fab fa-x"></i>
+            <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+                <Image src="/icons/discord-icon.svg" alt="Discord" width={0} height={0}
+                       style={{ width: '36px', height: 'auto' }} className="hover:opacity-75"/>
             </a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-500">
-                <i className="fab fa-github"></i>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <Image src="/icons/github-icon.svg" alt="GitHub" width={0} height={0} priority={true}
+                       style={{ width: '36px', height: 'auto' }} className="hover:opacity-75"/>
             </a>
         </div>
     );
 }
-
-export default SocialMediaIcons;
