ec9f1f4c2776d87ed6e2136e25bd8d5cd736c15d
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2025-02-24 16:35:13 +0100

    Improve metadata for link preview
    Will it work? Who knows.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,36 @@ import { DM_Sans } from "next/font/google";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "StreakyFly",
-  description: "StreakyFly's personal website. A place to share uhh... stuff.",
+    metadataBase: new URL(process.env.SITE_URL || "https://streakyfly.com"),
+    title: {
+        default: "StreakyFly's Legendary Site",
+        template: "%s | StreakyFly",
+    },
+    description: "StreakyFly's personal website. A place to share uhh... stuff.",
+    openGraph: {
+        title: "StreakyFly",
+        description: "StreakyFly's personal website. A place to share uhh... stuff.",
+        images: [{
+            url: "default-og.jpg",
+            width: 1200,
+            height: 630,
+        }],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        creator: "@streakyfly",
+        site: "@streakyfly",
+        images: "default-og.jpg"
+    }
 };
 
 export default function RootLayout({ children }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-        <body className={dmSans.className}>{children}</body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <body className={dmSans.className}>{children}</body>
+        </html>
+    );
 }
