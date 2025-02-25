18eb07d74ac10e362f329b6ce207873ee031e7a1
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2025-02-25 15:37:30 +0100

    Improve Blog object
    heck yeah we doin progress

--- src/database/models/Blog.ts
+++ src/database/models/Blog.ts
@@ -1,25 +1,27 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 import { ComponentUnion, ComponentSchema } from './Component';
 
-/**
- * OPTIONAL BLOG PROPERTIES:
- * - when did I start this project and when did I finish it: Date? (if it's a project)
- * - youtube video URL: string
- * - github repo URL: string
- * - project URL: string (e.g. live demo)
- * - collaborators: string[]
- * - notes (private): string (e.g. "This project was a pain to work on because of X, Y, Z")
- *
- * And possibly more...
- */
+export enum BlogStatus {
+    Draft = 'draft',
+    Private = 'private',
+    Public = 'public',
+}
+
+export enum BlogType {
+    Project = 'project',
+    Tutorial = 'tutorial',
+    Review = 'review',
+    // more to be added...
+}
 
 export interface Blog extends Document {
     title: string;
     description: string;
     coverImage: string;  // Cloudinary image ID or URL to image
     slug: string;
-    status: 'draft' | 'private' | 'public';
-    tags?: string[];
+    status: BlogStatus;
+    types: BlogType[];
+    tags: string[];
     createdAt: Date;  // automatically added by mongoose
     updatedAt: Date;  // automatically added by mongoose
     __v: number;      // automatically added by mongoose
@@ -39,13 +41,23 @@ const BlogSchema = new Schema<Blog>({
     status: {
         type: String,
         required: true,
-        enum: ['draft', 'private', 'public'],
-        default: 'draft',
+        enum: Object.values(BlogStatus),
+        default: BlogStatus.Draft,
     },
-    tags: { type: [String], required: false, },
-    components: { type: [ComponentSchema], required: false, },
+    types: {
+        type: [String],
+        required: true,
+        enum: Object.values(BlogType),
     },
-    {
+    tags: {
+        type: [String],
+        required: true,
+    },
+    components: {
+        type: [ComponentSchema],
+        required: false,
+    },
+    }, {
         timestamps: true,
         versionKey: '__v',
         strict: true,
