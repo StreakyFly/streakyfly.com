cfec499459d98f01ea36b9e3d2ce2cf4b02e34a5
Miha Ciglar <miha.ciglar7@gmail.com>
2025-04-06 16:51:27 +0200

    Prevent /admin from being indexed by search engines
    SEO stuff, here I go!

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,16 +1,22 @@
 import { ReactNode } from 'react';
+import Head from 'next/head';
 import Sidenav from '@/components/admin/Sidenav';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="flex">
-            <div>
-                <Sidenav />
+        <>
+            <Head>
+                <meta name="robots" content="noindex, nofollow" />
+            </Head>
+            <div className="flex">
+                <div>
+                    <Sidenav />
+                </div>
+                {/* border on all sides except the left, as Sidenav already takes care of that border */}
+                <div className="w-full h-screen overflow-y-auto border-t border-b border-r border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900">
+                    {children}
+                </div>
             </div>
-            {/* border on all sides except the left, as Sidenav already takes care of that border */}
-            <div className="w-full h-screen overflow-y-auto border-t border-b border-r border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900">
-                {children}
-            </div>
-        </div>
+        </>
     );
 }
